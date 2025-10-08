09756c4406c051479b89ac0baf5957bea87a7679
shreyash <shreyashkatiyaer2020@gmail.com>
2025-10-08 11:09:02 +0530

    shell structure

---
+++ shell.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+#define MAX_INPUT 1024
+
+int main(){
+    char input[MAX_INPUT];
+    while(1){
+        
+    printf("hst\n");
+    fflush(stdout);
+    if (fgets(input, MAX_INPUT, stdin)==NULL){
+        perror("fgets failed");
+        continue;
+    }
+    printf("%s",input);
+   
+    }
+    return 0;
+}
