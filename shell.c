e00443af91c3eac40a0c62c3757fdd61c5f4b544
shreyash <shreyashkatiyaer2020@gmail.com>
2025-10-08 11:42:44 +0530

    progress

--- shell.c
+++ shell.c
@@ -4,15 +4,24 @@
 
 int main(){
     char input[MAX_INPUT];
-    while(1){
-        
-    printf("hst\n");
-    fflush(stdout);
-    if (fgets(input, MAX_INPUT, stdin)==NULL){
-        perror("fgets failed");
-        continue;
-    }
-    printf("%s",input);
+    
+    while(1){            
+        printf("tsh>");
+        fflush(stdout);
+
+        if (fgets(input, MAX_INPUT, stdin) == NULL){
+            perror("fgets failed");
+            continue;
+        }
+
+        input[strcspn(input, "\n")] = '\0';
+
+        if (strcmp(input, "exit") == 0)    {
+            printf("have");
+            break;
+        }
+
+        printf("%s\n", input);
    
     }
     return 0;
