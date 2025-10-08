11803748811f97219ea71c120a826308f3fe3b67
shreyash <shreyashkatiyaer2020@gmail.com>
2025-10-08 18:56:25 +0530

    progress on 2nd level

--- shell.c
+++ shell.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
+#include<string.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #define MAX_INPUT 1024
+#define MAX_ARGS 64
 
 int main(){
     char input[MAX_INPUT];
+    char* args[MAX_ARGS];
     
     while(1){            
         printf("tsh>");
@@ -20,6 +25,23 @@ int main(){
             printf("have");
             break;
         }
+        char* token = strtok(input , " ");
+        int i=0;
+        while (token !=NULL && i < MAX_ARGS){
+            args[i++] = token;
+            token = strtok(NULL, " ");
+        }
+        args[i] = NULL;
+        
+        pid_t pid = fork(); 
+        if (pid = 0){
+            execvp(args[0], args);
+        }else if(pid > 0){
+
+        }else{
+            perror("fork failed");
+        }
+        
 
         printf("%s\n", input);
    
